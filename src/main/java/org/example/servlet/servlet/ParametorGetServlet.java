8ba1fdf874bab34ace7d97bc9cf8d93ea53248d4
Joel <joelcaza@hotmail.com>
2024-05-16 09:50:16 -0500

    Get

--- src/main/java/org/example/servlet/servlet/ParametorGetServlet.java
+++ src/main/java/org/example/servlet/servlet/ParametorGetServlet.java
@@ -8,37 +8,26 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-
 //path o llave para poder enlazar servlet
-@WebServlet("/servlet")
-public class Servlet  extends HttpServlet
-{
-    //Indicamos el metodo utilizar en el servlet, enviamos los datos mediante un link
+@WebServlet("/servletParametros")
+
+public class ParametorGetServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
-        out.println("<!DOCTYPE html>");
+        String nombre = req.getParameter("nombre");
+        String apellido = req.getParameter("apellido");
         out.println("<html>");
         out.println("<head>");
-        out.println("<meta charset=\"UTF-8\">");
-        out.println("<title>Servlet</title>");
+        out.println("<title>Servlet ParametorGetServlet</title>");
         out.println("</head>");
         out.println("<body>");
-        out.println("<h1>Servlet Get</h1>");
+        out.println("<h1>Servlet ParametorGetServlet</h1>");
+        out.println("<h2>Nombre: " + nombre + "</h2>");
+        out.println("<h2>Apellido: " + apellido + "</h2>");
         out.println("</body>");
         out.println("</html>");
-    }
-
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
-
-
-
-
 
     }
 }
