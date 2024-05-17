06ba17c1c74768bce5b38a49b545ea7727f79a71
Joel <joelcaza@hotmail.com>
2024-05-17 10:41:31 -0500

    Project

--- src/main/java/org/example/servlet/servlet/ParametorGetServlet.java
+++ src/main/java/org/example/servlet/servlet/ParametorGetServlet.java
@@ -1,5 +1,4 @@
 package org.example.servlet.servlet;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -11,34 +10,50 @@ import java.io.PrintWriter;
 //path o llave para poder enlazar servlet
 @WebServlet("/servletParametros")
 
+
+//nombre,edad,apellido,curso,radio button donde seleccione el genero,select con
+//lenguajes de programacion y un text area descripcion
 public class ParametorGetServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
         String nombre = req.getParameter("nombre");
         String apellido = req.getParameter("apellido");
-        Integer edad = Integer.valueOf(req.getParameter("edad"));
-        Double precio = Double.valueOf(req.getParameter("precio"));
-        Boolean estado = Boolean.valueOf(req.getParameter("estado"));
-        Float iva = Float.valueOf(req.getParameter("iva"));
-
-
+        String curso = req.getParameter("curso");
+        String genero = req.getParameter("genero");
+        String[] lenguajes = req.getParameterValues("lenguajes[]");
+        String descripcion = req.getParameter("descripcion");
 
         out.println("<html>");
         out.println("<head>");
-        out.println("<title>Servlet ParametorGetServlet</title>");
+        out.println("<title>Servlet Post</title>");
         out.println("</head>");
         out.println("<body>");
-        out.println("<h1>Servlet ParametorGetServlet</h1>");
+        out.println("<h1>Servlet Post</h1>");
         out.println("<h2>Nombre: " + nombre + "</h2>");
         out.println("<h2>Apellido: " + apellido + "</h2>");
-        out.println("<h2>Edad: " + edad + "</h2>");
-        out.println("<h2>Precio: " + precio + "</h2>");
-        out.println("<h2>EdadActual: " + estado + "</h2>");
-        out.println("<h2>Iva: " + iva + "</h2>");
+        out.println("<h2>Curso: " + curso + "</h2>");
+        out.println("<h2>Genero: " + genero + "</h2>");
+        out.println("<h2>Lenguajes de Programacion:</h2>");
+
+        if (lenguajes != null) {
+            for (String lenguaje : lenguajes) {
+                out.println("<li>" + lenguaje + "</li>");
+            }
+        } else {
+            out.println("<li>Ningún lenguaje seleccionado</li>");
+        }
+        out.println("<h2>Descripcion: " + descripcion + "</h2>");
+        out.println("<a href=\"servlet\" methods=\"get\">xd</a>");
         out.println("</body>");
         out.println("</html>");
 
     }
+
 }
